ba389741bfe1e603dcf8a6216b94354c26d1282c
Viktor <victorharasymiv97@gmail.com>
2025-01-08 21:52:31 +0100

    done

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './main.scss';
+import './scss/container.scss';
+import './scss/_normalize.scss';
+import './scss/_variables.scss';
+
+// P R O J E C T  S T Y L E 
+
+import './partials/header/header.scss';
+import './scss/hero.scss';
