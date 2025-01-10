b97484a7a9d58adf54dd885933b21cf5da793f77
Viktor <victorharasymiv97@gmail.com>
2025-01-10 03:16:39 +0100

    mobile menu

--- src/main.js
+++ src/main.js
@@ -6,6 +6,6 @@ import './scss/_variables.scss';
 // P R O J E C T  S T Y L E 
 
 import './partials/header/header.scss';
-import './scss/hero.scss';
+import './scss/modal.scss';
 import './scss/hero.scss';
 import './partials/footer/footer.scss';
