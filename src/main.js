09e635afc5ada104a04bd0d3906c9a10c48c24d3
Viktor <victorharasymiv97@gmail.com>
2025-01-10 23:11:30 +0100

    reload

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import './scss/modal.scss';
 import './scss/hero.scss';
 import './partials/footer/footer.scss';
 
-import './scripts/modal.js';
+import './scripts/modal';
 
 // function openModal() {
 //     const button = document.querySelector('.header--mobile_menu');
