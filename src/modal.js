1c9d0ed478133b522d8780b75f48df3d9b896cf6
Viktor <victorharasymiv97@gmail.com>
2025-01-10 04:01:49 +0100

    4

--- src/modal.js
+++
@@ -1,9 +0,0 @@
-function openModal() {
-    const button = document.querySelector('.header--mobile_menu');
-    const activePanel = document.querySelector('.header--mobile_menu_panel');
-    const activeBar = document.querySelector('.header--mobile_menu_navigation');
-
-    button.classList.toggle('is-active');
-    activePanel.classList.toggle('is-active');
-    activeBar.classList.toggle('is-active');
-}
